9b03ce10971dd16c1439553e28d924ac87fdcb97
Vasileios Charatsidis <charatsidisvasileios@gmail.com>
2019-04-09 22:45:32 +0200

    automation

--- Predict.py
+++ Predict.py
@@ -1,54 +1,148 @@
 f = open("Grubb.txt", "r")
 import numpy as np
 import pandas as pd
-saturation = 0
+
+
+def parse_one_hot(xin):
+    print(xin)
+    data = ""
+    if xin[0] ==1:
+        data += "Hum "
+    elif xin[1]==1:
+        data += " Ne "
+    elif xin[2]==1:
+        data += " Orc "
+    elif xin[3]==1:
+        data += " Ra "
+    elif xin[4]==1:
+        data += " Ud"
+
+    data += " vs "
+
+    if xin[5]==1:
+        data += " Hum "
+    elif xin[6]==1:
+        data += " Ne "
+    elif xin[7]==1:
+        data += " Orc "
+    elif xin[8]==1:
+        data += " Ra "
+    elif xin[9]==1:
+        data += " Ud"
+
+    # amazonia = 0
+    # concealed = 1
+    # echo = 2
+    # northren = 3
+    # refuge = 4
+    # swamped = 5
+    # terenas = 6
+    # turtle = 7
+    # twisted = 8
+    print("len xin "+str(len(xin)))
+    if len(xin) > 13:
+        if xin[10] == 1:
+            data += " amazonia "
+        elif xin[11] == 1:
+            data += " concealed "
+        elif xin[12] == 1:
+            data += " echo "
+        elif xin[13] == 1:
+            data += " northren "
+        elif xin[14] == 1:
+            data += " refuge"
+        elif xin[15] == 1:
+            data += " swamped "
+        elif xin[16] == 1:
+            data += " terenas "
+        elif xin[17] == 1:
+            data += " turtle "
+        elif xin[18] == 1:
+            data += " twisted"
+
+        if xin[19] == 1:
+            data += " tryhard "
+        else:
+            data += " request"
+
+        data += " "+str(xin[20])+"%"
+
+        data += " se "+str(xin[21])+" games"
+
+    else:
+        if xin[10] == 1:
+            data += " tryhard "
+        else:
+            data += " request"
+
+        data += " " + str(xin[11]) + "%"
+
+        data += " se " + str(xin[12]) + " games"
+
+    return data
 
 def parse_x(xin):
-    data = ""
-    if xin[1] == saturation+1:
-        data += "tryhard "
+    data = "1-"
+
+
+    if xin[0] == 0:
+        data += "Hum-"
+    elif xin[0] == 1:
+        data += "Ne-"
+    elif xin[0] == 2:
+        data += "Orc-"
+    elif xin[0] == 3:
+        data += "Ra-"
+    elif xin[0] == 4:
+        data += "Ud-"
+
+    if xin[1] == 1:
+        data += "t-"
     else:
-        data +="request "
+        data += "r-"
 
-    if xin[0] == saturation+1:
-        data +="Ne"
-    elif xin[0] == saturation+2:
-        data +="Orc"
-    elif xin[0] == saturation+3:
-        data +="Random"
-    elif xin[0] == saturation+4:
-        data +="Undead"
-    else:
-        data +="Human"
+    if xin[2] == 0:
+        data += "Hum-"
+    elif xin[2] == 1:
+        data += "Ne-"
+    elif xin[2] == 2:
+        data += "Orc-"
+    elif xin[2] == 3:
+        data += "Ra-"
+    elif xin[2] == 4:
+        data += "Ud-"
 
-    data +=" vs "
-
-    if xin[2] == saturation+1:
-        data +="Ne"
-    elif xin[2] == saturation+2:
-        data +="Orc"
-    elif xin[2] == saturation+3:
-        data +="Random"
-    elif xin[2] == saturation+4:
-        data +="Undead"
-    else:
-        data +="Human"
-
-    data += " "
     data += str(xin[3])
-    data += " games "
+    data += "-"+str(xin[4])+"-"
 
-    if xin[4]== saturation+1:
-        data += " less than 20"
-    elif xin[4]== saturation+2:
-        data += " less than 60"
-    elif xin[4]== saturation+3:
-        data += " less than 120"
-    else:
-        data += "many"
-
-    # data +=" map "
-    # data += str(xin[5])
+    # amazonia = 0
+    # concealed = 1
+    # echo = 2
+    # northren = 3
+    # refuge = 4
+    # swamped = 5
+    # terenas = 6
+    # turtle = 7
+    # twisted = 8
+    if(len(xin)>5):
+        if xin[5] == 0:
+            data += "amazonia"
+        elif xin[5] == 1:
+            data += "concealed"
+        elif xin[5] == 2:
+            data += "echo"
+        elif xin[5] == 3:
+            data += "northren"
+        elif xin[5] == 4:
+            data += "refuge"
+        elif xin[5] == 5:
+            data += "swamped"
+        elif xin[5] == 6:
+            data += "terenas"
+        elif xin[5] == 7:
+            data += "turtle"
+        elif xin[5] == 8:
+            data += "twisted"
 
     return data
 
@@ -56,13 +150,13 @@ def less_buckets(games):
     bucket = 0
 
     if games <= 20:
-        bucket = saturation+1
+        bucket = 1
     elif games <= 60:
-        bucket = saturation+2
+        bucket = 2
     elif games <= 150:
-        bucket = saturation+3
+        bucket = 3
     else:
-        bucket = saturation+4
+        bucket = 4
 
     return bucket
 
@@ -91,21 +185,26 @@ counter = 0
 for l in contents:
 
     X = l.split('-')
-    z = int(X[4])
-    stats_adipalou = (round(z/5))*5
-    X[4] = stats_adipalou
-    games_adipalou = int(X[5])
-    b = less_buckets(games_adipalou)
-    X[5] = b
+    # z = int(X[4])
+    # stats_adipalou = (round(z/5))*5
+    # X[4] = stats_adipalou
+    # games_adipalou = int(X[5])
+    # b = less_buckets(games_adipalou)
+    # X[5] = b
+    # X[6] = X[6].rstrip("\n")
+
+    X[4] = int(X[4])
+    X[5] = int(X[5])
     X[6] = X[6].rstrip("\n")
 
 
     X = np.array(X)
     input.append(X)
 
-    print("bucket "+str(b))
-    print("stats adipalou rounded "+str(stats_adipalou))
-    print("stats adipalou "+str(z))
+    # print("bucket "+str(b))
+    # print("stats adipalou rounded "+str(stats_adipalou))
+    # print("stats adipalou "+str(z))
+
     print(X)
     print(l)
     counter += 1
@@ -125,32 +224,22 @@ print(input[18])
 # input = labelencoder.fit_transform(input)
 # print(input)
 input[:, 0] = labelencoder.fit_transform(input[:, 0])
-input[:, 0] = [int(x)+ saturation for x in input[:, 0]]
+input[:, 0] = [int(x) for x in input[:, 0]]
 input[:, 1] = labelencoder.fit_transform(input[:, 1])
-input[:, 1] = [int(x)+saturation for x in input[:, 1]]
+input[:, 1] = [int(x) for x in input[:, 1]]
 input[:, 2] = labelencoder.fit_transform(input[:, 2])
-input[:, 2] = [int(x)+saturation for x in input[:, 2]]
+input[:, 2] = [int(x) for x in input[:, 2]]
 # #input[:, 3] = labelencoder.fit_transform(input[:, 3])
 # input[:, 4] = labelencoder.fit_transform(input[:, 4])
 input[:, 5] = labelencoder.fit_transform(input[:, 5])
-input[:, 5] = [int(x)+saturation for x in input[:, 5]]
+input[:, 5] = [int(x) for x in input[:, 5]]
 print(input)
-
-
-
-# onehotencoder = OneHotEncoder(categorical_features=[0, 2, 5])
-# input = onehotencoder.fit_transform(input).toarray()
-
-
 from sklearn.ensemble import RandomForestClassifier, RandomForestRegressor
-classifier = RandomForestClassifier(n_estimators=3000, random_state=0)
+import copy
+input2 = copy.deepcopy(input)
+
+classifier = RandomForestClassifier(n_estimators=5000, random_state=0)
 classifier.fit(input, y)
-
-number = len(input)-1
-
-print("number "+str(number) + " : "+str(input[number]))
-
-print("result "+str(y[number]))
 
 # amazonia = 0
 # concealed = 1
@@ -162,8 +251,9 @@ print("result "+str(y[number]))
 # turtle = 7
 # twisted = 8
 # --------------------------------------------- Input -----------------------------------------------------------
-xin = [saturation+4, saturation+1, saturation+0, 75, saturation+4, saturation+3]
-
+xin = [0, 1, 4, 60, 430, 2]
+to_print = copy.deepcopy(xin)
+print(parse_x(to_print))
 # Hum = 0
 # Ne = 1
 # Orc = 2
@@ -174,51 +264,116 @@ xin = [saturation+4, saturation+1, saturation+0, 75, saturation+4, saturation+3]
 # games less than 60 = 2
 # games less than 120 = 3
 # many games = 4
+
+
+
+onehot_encoded = []
+
+letter = [0 for _ in range(5)]
+letter[xin[0]] = 1
+
+for i in letter:
+    onehot_encoded.append(i)
+
+letter = [0 for _ in range(5)]
+letter[xin[2]] = 1
+for i in letter:
+    onehot_encoded.append(i)
+
+letter = [0 for _ in range(9)]
+letter[xin[5]] = 1
+for i in letter:
+    onehot_encoded.append(i)
+
+onehot_encoded = np.array(onehot_encoded)
+onehot_encoded.flatten()
+
+onehot_encoded = np.append(onehot_encoded, xin[1])
+onehot_encoded = np.append(onehot_encoded, xin[3])
+onehot_encoded = np.append(onehot_encoded, xin[4])
+
+onehot_encoded.flatten()
+
+
+print("manual one hot "+str(onehot_encoded))
+
+# print(parse_one_hot(xin))
+
+y_pred1 = classifier.predict_proba([xin])
 print(xin)
-print("input "+str(xin))
-print(parse_x(xin))
-print(input[-1])
-print("")
-
-# xin = [[0., 0., 1., 0., 0.,      1., 0., 0., 0., 0.,   0., 1., 0., 0., 0., 0., 0., 0., 0., 0.,  saturation+1,      85, saturation+4]]
-# print("one hot in "+str(xin))
-
-y_pred = classifier.predict_proba([xin])
-print(xin)
-print("Chanses that Grubby wins "+str(y_pred[0][1]*100)+"%")
-print("probabilities "+str(y_pred[0]))
-
-
-regressor = RandomForestRegressor(n_estimators=3000, random_state=0)
-regressor.fit(input, y)
-y_reg = regressor.predict([xin])
-print("regressor "+str(y_reg))
+print("Chanses that Grubby wins "+str(y_pred1[0][1]*100)+"%")
 
 #   Without map prediction   ##############################################
 
 print("")
 print("Without map prediciton ")
-print("")
 input = input[:, :-1]
-
-# xin = [0., 1., 0., 0., 0.,        1., 0.,        1., 0., 0., 0., 0.,          70, saturation+4]
-# print("one hot in "+str(xin))
-# onehotencoder = OneHotEncoder(categorical_features=[0, 1, 2])
-# input = onehotencoder.fit_transform(input).toarray()
-
-print("number "+str(number)+" : "+str(input[number]))
-
 classifier.fit(input, y)
-regressor.fit(input, y)
-
 xin = xin[:-1]
-print(" ")
-print(parse_x(xin))
+y_pred2 = classifier.predict_proba([xin])
+print("Chanses that Grubby wins "+str(y_pred2[0][1]*100)+"%")
+
+print("")
+print("One hot ")
+print("")
+
+# One Hot ############################################################
+
+# amazonia = 0
+# concealed = 1
+# echo = 2
+# northren = 3
+# refuge = 4
+# swamped = 5
+# terenas = 6
+# turtle = 7
+# twisted = 8
 
 
-y_pred = classifier.predict_proba([xin])
-print("Chanses that Grubby wins "+str(y_pred[0][1]*100)+"%")
-print("probabilities "+str(y_pred[0]))
 
-y_reg = regressor.predict([xin])
-print("regressor "+str(y_reg))
+
+
+#xin = [1., 0., 0., 0., 0.,      0., 1., 0., 0., 0.,   0., 0., 0., 1., 0., 0., 0., 0., 0.,       1,      89, 430]
+
+# Hum = 0
+# Ne = 1
+# Orc = 2
+# Ra = 3
+# Ud = 4
+
+onehotencoder = OneHotEncoder(categorical_features=[0, 2, 5])
+onehot_input = onehotencoder.fit_transform(input2).toarray()
+
+classifier2 = RandomForestClassifier(n_estimators=5000, random_state=0)
+classifier2.fit(onehot_input, y)
+xin = onehot_encoded
+y_pred3 = classifier2.predict_proba([xin])
+print("Chanses that Grubby wins "+str(round(y_pred3[0][1]*100))+"%")
+
+
+xin2 =[xin[0],  xin[1], xin[2], xin[3], xin[4], xin[5], xin[6], xin[7], xin[8], xin[9], xin[19], xin[20], xin[21]]
+
+input2 = input2[:, :-1]
+onehotencoder = OneHotEncoder(categorical_features=[0,  2])
+onehot_input = onehotencoder.fit_transform(input2).toarray()
+
+print("")
+print("Without map prediciton ")
+
+classifier3 = RandomForestClassifier(n_estimators=5000, random_state=0)
+classifier3.fit(onehot_input, y)
+y_pred4 = classifier3.predict_proba([xin2])
+
+print(parse_one_hot(xin))
+print("Chanses that Grubby wins "+str(y_pred4[0][1]*100)+"%")
+
+
+print(" pred map "+str(round(y_pred1[0][1]*100))+"%"+" pred 2 "+str(round(y_pred2[0][1]*100))+"%"+" onehot map "+
+      str(round(y_pred3[0][1]*100))+"%"+" onehot 3 "+str(round(y_pred4[0][1]*100))+"%")
+
+print(parse_x(to_print))
+s = parse_x(to_print)
+print(s+"-"+str(int(round(y_pred1[0][1]*100)))
+      +"%-"+str(int(round(y_pred2[0][1]*100)))
+      +"%-"+str(int(round(y_pred3[0][1]*100)))
+      +"%-"+str(int(round(y_pred4[0][1]*100)))+"%-")
